cb6309f9b3ca72f788356e4e2a1b42964628d732
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-01-31 14:54:25 +0530

    Some more changes

--- benchmark.sh
+++ benchmark.sh
@@ -3,20 +3,14 @@
 # Specify the output file
 output_file="output_2.txt"
 
-echo "Running benchmark for hnsw_pq with pq_m = 4, pq_bits = 16"
+echo "Running benchmark for hnsw_pq with pq_m = 16, pq_bits = 10"
 ./build/release/bin/vector_index_main -f /home/gaurav/vector_index_experiments/gist -k 100 -t hnsw_pq -efConstruction 100 -m 64 -pq_m 16 -pq_bits 16 -efSearch 128 -nIndexingThreads 16 -nSearchThreads 2,4,8,16 >> "$output_file"
 
-echo "Running benchmark for hnsw_pq with pq_m = 16, pq_bits = 16"
+echo "Running benchmark for hnsw_pq with pq_m = 16, pq_bits = 12"
 ./build/release/bin/vector_index_main -f /home/gaurav/vector_index_experiments/gist -k 100 -t hnsw_pq -efConstruction 100 -m 64 -pq_m 60 -pq_bits 16 -efSearch 128 -nIndexingThreads 16 -nSearchThreads 2,4,8,16 >> "$output_file"
 
-echo "Running benchmark for hnsw_pq with pq_m = 4, pq_bits = 32"
+echo "Running benchmark for hnsw_pq with pq_m = 30, pq_bits = 10"
 ./build/release/bin/vector_index_main -f /home/gaurav/vector_index_experiments/gist -k 100 -t hnsw_pq -efConstruction 100 -m 64 -pq_m 16 -pq_bits 32 -efSearch 128 -nIndexingThreads 16 -nSearchThreads 2,4,8,16 >> "$output_file"
 
-echo "Running benchmark for hnsw_pq with pq_m = 16, pq_bits = 32"
+echo "Running benchmark for hnsw_pq with pq_m = 30, pq_bits = 12"
 ./build/release/bin/vector_index_main -f /home/gaurav/vector_index_experiments/gist -k 100 -t hnsw_pq -efConstruction 100 -m 64 -pq_m 60 -pq_bits 32 -efSearch 128 -nIndexingThreads 16 -nSearchThreads 2,4,8,16 >> "$output_file"
-
-echo "Running benchmark for hnsw_pq with pq_m = 4, pq_bits = 64"
-./build/release/bin/vector_index_main -f /home/gaurav/vector_index_experiments/gist -k 100 -t hnsw_pq -efConstruction 100 -m 64 -pq_m 16 -pq_bits 64 -efSearch 128 -nIndexingThreads 16 -nSearchThreads 2,4,8,16 >> "$output_file"
-
-echo "Running benchmark for hnsw_pq with pq_m = 8, pq_bits = 64"
-./build/release/bin/vector_index_main -f /home/gaurav/vector_index_experiments/gist -k 100 -t hnsw_pq -efConstruction 100 -m 64 -pq_m 60 -pq_bits 64 -efSearch 128 -nIndexingThreads 16 -nSearchThreads 2,4,8,16 >> "$output_file"
